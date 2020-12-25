01338b6b03338cea46b879afcd65ce061073f240
Телков Д.С <TelkovDS@pba.su>
2020-12-25 16:19:38 +0300

    Отключил стилизатор

--- lib.asiopq/asiopq/layer3/dump_result.hpp
+++ lib.asiopq/asiopq/layer3/dump_result.hpp
@@ -5,10 +5,8 @@
 #include <boost/system/error_code.hpp>
 
 
-namespace ba
-{
-namespace asiopq
-{
+namespace ba {
+namespace asiopq {
 
 class DumpResult
 {
@@ -25,7 +23,7 @@ public:
         m_opt.header = 1;
         m_opt.align = 1;
         m_opt.expanded = 1;
-        m_opt.fieldSep = const_cast<char*>(", ");
+        m_opt.fieldSep = const_cast<char*>()", ";
     }
 
     boost::system::error_code operator()(const ::PGresult* res) const noexcept
